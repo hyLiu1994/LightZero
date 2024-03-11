9de676a8779229f254ed093ab28d8ba00bd6afa8
Hengyu Liu <heli@cs.aau.dk>
2024-03-11 18:01:51 +0100

    add: add navie adversary for muzero

--- lzero/worker/__init__.py
+++ lzero/worker/__init__.py
@@ -2,3 +2,4 @@ from .alphazero_collector import AlphaZeroCollector
 from .alphazero_evaluator import AlphaZeroEvaluator
 from .muzero_collector import MuZeroCollector
 from .muzero_evaluator import MuZeroEvaluator
+from .muzero_adversary_collector import MuZeroAdversaryCollector
