b6c7d4d9554bce18250a0f2058ece993adaffa85
HeYQ <hyqitjavaee@163.com>
2024-05-28 22:58:48 +0800

    更新

--- lzero/entry/__init__.py
+++ lzero/entry/__init__.py
@@ -2,7 +2,7 @@ from .train_alphazero import train_alphazero
 from .eval_alphazero import eval_alphazero
 from .train_muzero import train_muzero
 from .train_muzero_with_adversary import train_muzero_with_adversary
-from .train_muzero_with_two_adversary import train_muzero_two_with_adversary
+from .train_muzero_with_two_adversary import train_muzero_with_two_adversary
 from .train_muzero_with_reward_model import train_muzero_with_reward_model
 from .eval_muzero import eval_muzero
 from .eval_muzero_with_gym_env import eval_muzero_with_gym_env
