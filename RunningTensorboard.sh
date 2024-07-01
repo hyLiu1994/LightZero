2d3be65611f3298eddcdaeb510705c24d9640c8b
Hengyu Liu <l372511387@163.com>
2024-07-01 22:57:11 +0800

    update: update config file

--- RunningTensorboard.sh
+++ RunningTensorboard.sh
@@ -1,2 +1,2 @@
 # tensorboard --logdir=/nfs/srv/data2/hengyu/LightZero/data_sez_ctree/ --host=172.19.8.25 --port 6009
-tensorboard --logdir=/root/autodl-tmp/LightZero/data_sez_ctree/ --port 6009
+tensorboard --logdir=/root/autodl-tmp/LightZero/data_sez_ctree/ --port 6005
