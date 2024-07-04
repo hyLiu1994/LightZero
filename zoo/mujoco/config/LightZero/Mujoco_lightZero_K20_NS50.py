0e91c4f6a24a8479eab8f2cfffe995e309204b8d
Hengyu Liu <heli@cs.aau.dk>
2024-07-04 13:58:47 +0200

    update: update config file

--- zoo/mujoco/config/LightZero/Mujoco_lightZero_K20_NS50.py
+++ zoo/mujoco/config/LightZero/Mujoco_lightZero_K20_NS50.py
@@ -1,9 +1,11 @@
 from easydict import EasyDict
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # options={'Hopper-v3', 'HalfCheetah-v3', 'Walker2d-v3', 'Ant-v3', 'Humanoid-v3'}
-env_id = 'Hopper-v3'
+env_id = 'Hopper-v2'
 
-if env_id == 'Hopper-v3':
+if env_id == 'Hopper-v2':
     action_space_size = 3
     observation_shape = 11
 elif env_id in ['HalfCheetah-v3', 'Walker2d-v3']:
